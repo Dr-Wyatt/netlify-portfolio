b06dbe576a079c17cb3143574c4c91aaabce2f71
Dr-Wyatt <daniel.ryuichi.wyatt@gmail.com>
2021-07-22 14:07:57 -0500

    Added in drawer functionality

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,13 +1,117 @@
-import { AppBar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Divider,
+  Drawer,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Theme,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
 import React from "react";
 
+const userStyles = makeStyles((theme: Theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(1),
+  },
+  appBar: {
+    background: theme.palette.primary.light,
+    color: theme.palette.primary.dark,
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: "auto",
+  },
+}));
+
+type Anchor = "right";
+
 const Navbar: React.FC = () => {
-    
+  const classes = userStyles();
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+  const list = (anchor: Anchor) => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
   return (
-    <AppBar position="static">
-      <Typography>
-        Daniel Wyatt
-      </Typography>
+    <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar>
+        <Grid container xs={12} justify="flex-end">
+          <React.Fragment key="right">
+            <IconButton
+              edge="end"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+              onClick={toggleDrawer("right", true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            {/* <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button> */}
+            <Drawer
+              anchor="right"
+              open={state["right"]}
+              onClose={toggleDrawer("right", false)}
+            >
+              {list("right")}
+            </Drawer>
+          </React.Fragment>
+        </Grid>
+      </Toolbar>
     </AppBar>
   );
 };
--- src/Components/ProjectCardList.tsx
+++ src/Components/ProjectCardList.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { ProjectCardInterface } from "../Interfaces/projectCard";
 import ProjectCard from "./ProjectCard";
 
-const ProjectCardList = ({info}: {info: ProjectCardInterface[]}) => {
+const ProjectCardList = ({ info }: { info: ProjectCardInterface[] }) => {
   return (
     <Grid
       container
@@ -14,26 +14,24 @@ const ProjectCardList = ({info}: {info: ProjectCardInterface[]}) => {
         height: "50vh",
       }}
     >
-      {info.map((cardInfo: ProjectCardInterface) => {
-        return (
-          <Grid
-            item
-            xs={3}
-            style={{ padding: "10px", justifyContent: "center" }}
-            key={info.indexOf(cardInfo)}
-          >
-            <ProjectCard
-              title={cardInfo.title}
-              appDescription={cardInfo.appDescription}
-              appLink={cardInfo.appLink}
-              explored={cardInfo.explored}
-              iconURL={cardInfo?.iconURL}
-              homePage={cardInfo?.homePage}
-              appInfo={cardInfo?.appInfo}
-            />
-          </Grid>
-        );
-      })}
+      {info.map((cardInfo: ProjectCardInterface) => (
+        <Grid
+          item
+          xs={3}
+          style={{ padding: "10px", justifyContent: "center" }}
+          key={info.indexOf(cardInfo)}
+        >
+          <ProjectCard
+            title={cardInfo.title}
+            appDescription={cardInfo.appDescription}
+            appLink={cardInfo.appLink}
+            explored={cardInfo.explored}
+            iconURL={cardInfo?.iconURL}
+            homePage={cardInfo?.homePage}
+            appInfo={cardInfo?.appInfo}
+          />
+        </Grid>
+      ))}
     </Grid>
   );
 };
