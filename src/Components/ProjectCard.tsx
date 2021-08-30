b5ba136d69e57573b18c9ae2872f5b621284db11
Daniel Wyatt <Daniel.Wyatt@echo.com>
2021-08-30 12:11:52 -0500

    Added in more card info details

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -1,23 +1,51 @@
 import React from "react";
 import { AnimateSharedLayout, motion } from "framer-motion";
 import { useState } from "react";
-import { Card, makeStyles, Typography } from "@material-ui/core";
+import {
+  Card,
+  Grid,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+import ArrowForwardIosRoundedIcon from "@material-ui/icons/ArrowForwardIosRounded";
 import "../Styling/project-card.css";
 import { ProjectCardInterface } from "../Interfaces/projectCard";
 
 const font = "'Poppins', sans-serif";
 
 const userStyles = makeStyles(() => ({
-  typography: {
+  normalCardTitle: {
     fontFamily: font,
     fontSize: 25,
   },
+  expandedCardTitle: {
+    fontFamily: font,
+    fontSize: 35,
+    color: "white",
+    margin: "auto",
+  },
+  infoText: {
+    fontFamily: font,
+    fontSize: 16,
+    color: "white",
+    verticalAlign: "middle",
+  },
+  links: {
+    fontFamily: font,
+    fontSize: 16,
+    color: "white",
+    textDecoration: "none",
+  },
 }));
+
 
 const ProjectCard: React.FC<ProjectCardInterface> = (cardInfo) => {
   const classes = userStyles();
   const [open, setOpen] = useState(false);
-  console.log("this is cardInfo in project card: ", cardInfo);
   return (
     <AnimateSharedLayout>
       {open ? (
@@ -32,18 +60,84 @@ const ProjectCard: React.FC<ProjectCardInterface> = (cardInfo) => {
             layoutY: { duration: 0.2 },
           }}
         >
-          <motion.h2
-            className="expanded-card-h"
-            layoutId="expandable-card-h"
-            layout
-            transition={{
-              layoutX: { duration: 0.2 },
-              layoutY: { duration: 0.2 },
-            }}
+          <Grid
+            container
+            xs={12}
+            justify="center"
+            style={{ marginTop: "100px" }}
           >
-            {cardInfo.title}
-          </motion.h2>
-          <p>{cardInfo.appDescription}</p>
+            <Grid container item xs={6} style={{ paddingRight: "10px" }}>
+              <Grid container item xs={12} justify="flex-end">
+                <motion.h2
+                  className="expanded-card-h"
+                  layoutId="expandable-card-h"
+                  layout
+                  transition={{
+                    layoutX: { duration: 0.2 },
+                    layoutY: { duration: 0.2 },
+                  }}
+                >
+                  <Typography className={classes.expandedCardTitle}>
+                    {cardInfo.title}
+                  </Typography>
+                </motion.h2>
+              </Grid>
+              <Grid container item direction="column" xs={12}>
+                <Grid container item xs={12} justify="flex-end">
+                  <a
+                    href={cardInfo.homePage}
+                    target="_blank"
+                    rel="noreferrer"
+                    className={classes.links}
+                  >
+                    Homepage
+                  </a>
+                </Grid>
+                <Grid container item xs={12} justify="flex-end">
+                  <a
+                    href={cardInfo.appLink}
+                    target="_blank"
+                    rel="noreferrer"
+                    className={classes.links}
+                  >
+                    Staging Ground
+                  </a>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              item
+              xs={6}
+              alignItems="center"
+              justify="flex-start"
+            >
+              <Grid item xs={8}>
+                <Typography className={classes.infoText}>
+                  <p>{cardInfo.appDescription}</p>
+                </Typography>
+                <List dense={true}>
+                  {cardInfo.explored.map((text) => {
+                    return (
+                      <ListItem>
+                        <ListItemIcon style={{ justifyContent: "flex-end" }}>
+                          <ArrowForwardIosRoundedIcon
+                            style={{ color: "white", paddingRight: "5px" }}
+                          />
+                        </ListItemIcon>
+                        <ListItemText
+                          disableTypography
+                          className={classes.infoText}
+                          primary={text}
+                        />
+                      </ListItem>
+                    );
+                  })}
+                </List>
+                ;
+              </Grid>
+            </Grid>
+          </Grid>
         </motion.div>
       ) : (
         /* NORMAL CARD */
@@ -55,19 +149,20 @@ const ProjectCard: React.FC<ProjectCardInterface> = (cardInfo) => {
             layoutId="expandable-card"
             layout
             transition={{
-              layoutX: { duration: 0.3 },
-              layoutY: { delay: 0.2, duration: 0.3 },
+              layoutX: { duration: 0.2 },
+              layoutY: { duration: 0.2 },
             }}
           >
             <motion.h1
               layout
+              className="normal-card"
               layoutId="expandable-card-h"
               transition={{
                 layoutX: { duration: 0.2 },
                 layoutY: { duration: 0.2 },
               }}
             >
-              <Typography className={classes.typography}>
+              <Typography className={classes.normalCardTitle}>
                 {cardInfo.title}
               </Typography>
             </motion.h1>
